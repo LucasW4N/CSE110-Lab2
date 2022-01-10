adfbf510780ec066a1da600243b2af6210fa18f2
unknown <wanzhangwen@gmail.com>
2022-01-10 14:39:21 -0800

    my first commit

--- app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// making a little change
 public class MainActivity extends AppCompatActivity {
 
     @Override
